292cf897371247ceac5aeebe3811f219ab175de6
mtx-testfan <3512937625@qq.com>
2020-09-16 11:42:48 +0800

    加入了os和sys

--- run/run_suite.py
+++ run/run_suite.py
@@ -1,3 +1,7 @@
+import os
+import sys
+o_path = os.path.abspath(os.path.join(os.getcwd(), "../"))
+sys.path.append(o_path)
 import unittest
 # 路径都是相对路径 ->相对的是你的入口文件，执行文件
 from tool.HTMLTestRunner import HTMLTestRunner

9c7a26865c62f7313b61fdab5eadb544f48e8b64
mtx-testfan <3512937625@qq.com>
2020-09-16 13:25:27 +0800

    生成没有报告的

--- run/run_suite.py
+++ run/run_suite.py
@@ -9,6 +9,8 @@ from tool.HTMLTestRunner import HTMLTestRunner
 suite = unittest.defaultTestLoader.discover('script')
 path = os.path.dirname(__file__)
 base_path = os.path.dirname(path)
-with open(base_path + '/report/mtx_shop.html', 'wb') as f:
-    HTMLTestRunner(stream=f,title='码同学商场报告',description='码同学商城的登录用例的执行').run(suite)
+# with open(base_path + '/report/mtx_shop.html', 'wb') as f:
+#     HTMLTestRunner(stream=f,title='码同学商场报告',description='码同学商城的登录用例的执行').run(suite)
 
+runner = unittest.TextTestRunner()
+runner.run(suite)

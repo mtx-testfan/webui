e4aac12b68b8a46f46c65d4824ba570b908084aa
mtx-testfan <3512937625@qq.com>
2020-09-16 13:15:58 +0800

    调试了一下路径，动态生成报告

--- run/run_suite.py
+++ run/run_suite.py
@@ -1,13 +1,14 @@
 import os
 import sys
-o_path = os.path.abspath(os.path.join(os.getcwd(), "../"))
+o_path = os.path.abspath(os.path.join(os.getcwd(), "./"))
 sys.path.append(o_path)
 import unittest
 # 路径都是相对路径 ->相对的是你的入口文件，执行文件
 from tool.HTMLTestRunner import HTMLTestRunner
 
 suite = unittest.defaultTestLoader.discover('script')
-
-with open('../report/mtx_shop.html', 'wb') as f:
+path = os.path.dirname(__file__)
+base_path = os.path.dirname(path)
+with open(base_path + '/report/mtx_shop.html', 'wb') as f:
     HTMLTestRunner(stream=f,title='码同学商场报告',description='码同学商城的登录用例的执行').run(suite)
 

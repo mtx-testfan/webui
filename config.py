1a0b1318036552667618edc1fbc8ff002031438e
mtx-testfan <3512937625@qq.com>
2020-09-16 14:42:30 +0800

    htmltestrunner

---
+++ config.py
@@ -0,0 +1,3 @@
+import os
+PATH = os.path.abspath(__file__)
+BASE_PATH = os.path.dirname(PATH)
--- run/run_suite.py
+++ run/run_suite.py
@@ -1,16 +1,18 @@
 import os
 import sys
+
+
+
 o_path = os.path.abspath(os.path.join(os.getcwd(), "./"))
 sys.path.append(o_path)
+import config
 import unittest
 # 路径都是相对路径 ->相对的是你的入口文件，执行文件
 from tool.HTMLTestRunner import HTMLTestRunner
 
 suite = unittest.defaultTestLoader.discover('script')
-path = os.path.dirname(__file__)
-base_path = os.path.dirname(path)
-# with open(base_path + '/report/mtx_shop.html', 'wb') as f:
-#     HTMLTestRunner(stream=f,title='码同学商场报告',description='码同学商城的登录用例的执行').run(suite)
+with open(config.BASE_PATH + '/report/mtx_shop.html', 'wb') as f:
+    HTMLTestRunner(stream=f,title='码同学商场报告',description='码同学商城的登录用例的执行').run(suite)
 
-runner = unittest.TextTestRunner()
-runner.run(suite)
+# runner = unittest.TextTestRunner()
+# runner.run(suite)

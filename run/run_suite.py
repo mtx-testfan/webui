4ba0968df9c482ca0219f074ab7934e7be2f8a71
mtx-testfan <3512937625@qq.com>
2020-09-16 11:21:34 +0800

    删除from script这个导包，因为没有用到

--- run/run_suite.py
+++ run/run_suite.py
@@ -1,6 +1,5 @@
 import unittest
 # 路径都是相对路径 ->相对的是你的入口文件，执行文件
-from script.test_login import Login
 from tool.HTMLTestRunner import HTMLTestRunner
 
 suite = unittest.defaultTestLoader.discover('script')
